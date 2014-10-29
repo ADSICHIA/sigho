b972555d6946e29b4e680c3a88612f6804c14797
oscar vizcaino <oavizcaino@misena.edu.co>
2014-10-29 17:51:43 -0500

    ajiste mvc grupos

--- vista/vlistagrupo.php
+++ vista/vlistagrupo.php
@@ -32,7 +32,7 @@
                                                      <td><?php echo $datos[$i]['ambiente'] ?></td>
                                                 
                                                     <td><a href="home.php?pac=113&in=2&pr=<?php echo $sedes[$i]['idsede'] ?>"><span class="glyphicon glyphicon-edit"></span></a></td>
-                                                    <td><a href="home.php?pac=114&del=<?php echo $sedes[$i]['idsede'] ?>" onclick="return confirm('¿Desea eliminar la sede?');"><span class="glyphicon glyphicon-trash"></span></a></td>
+                                                    <td><a href="home.php?pac=111&del=<?php echo $datos[$i]['idgrupo'] ?>" onclick="return confirm('¿Desea eliminar la sede?');"><span class="glyphicon glyphicon-trash"></span></a></td>
                                                 </tr>
                                                 <?php } ?>
                                             </tbody>
