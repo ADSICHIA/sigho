5d9fc3bac2c5e3bacfdb1d999f2524b2236ceadd
cmvelasquez <cmvelasquez974@misena.edu.co>
2014-11-08 12:11:08 -0500

    ultima modificacion a disponibilidad

--- vista/Disponibilidad.php
+++ vista/Disponibilidad.php
@@ -1,96 +1,88 @@
 <?php
 include ("controlador/cdisponi.php");
-
 ?>
 <html>
-<head>
-    
-<title>Disponibilidad</title>
-</head>
-<title>Sujerencia de Disponibilidad</title>
-<body>
-    
- <div class="table-responsive">    
- <h3>DISPONIBILIDAD SUJERIDA</h3>   
-<form id="usuario" name="Disponibilidad" method="post" action="">
-    
-    <table width="600" height="200">
+    <head>
 
-        <tr>
-<!--                    <td align="left">Nombre del usuario: </td>
-                    <td> <select name="usuarios" onchange="this.form.submit();">
+        <title>Disponibilidad</title>
+    </head>
+    <title>Sujerencia de Disponibilidad</title>
+    <body>
 
-                            <?php
-                            //Select
-                            $usu = $ins->selusuario();
-                            for ($h = 0; $h < count($usu); $h++) {
+        <div class="table-responsive">   
+
+            <h3 aling="center" style="text-align: center">DISPONIBILIDAD SUGERIDA</h3>
+
+            <form id="usuario" name="Disponibilidad" method="post" >
+
+
+                <table width="600" height="200" id ="tdispo" style="position: relative; left: 220px">
+
+
+                    <tr  bordercolor="#FFFFFF" font="weight:bold" style="background:#2E64FE;opacity: 0.8;">
+                        <td align="center" width="100" style="border: 3px solid #EFF5FB ">Jornada</td>
+
+                        <?php
+                        $dia = $ins->seldias();
+                        for ($i = 0; $i < count($dia); $i++) {
+                            ?>
+                            <td align="center" width="100" style="border: 3px solid #EFF5FB "><?php echo $dia[$i]['valor']; ?></td>
+                        <?php } ?>
+                    </tr>
+
+                    <?php
+                    $dispo = $ins->seldisponi($usuarioid);
+
+
+                    $jorn = $ins->seljornadas();
+
+                    for ($j = 0; $j < count($jorn); $j++) {
+                        ?>
+
+                        <tr>
+                            <td style="border-bottom: 1px solid #3730B9; background:#2E64FE;opacity: 0.8;border: 3px solid #EFF5FB ;" align="center" ><?php
+                    $date = date_create($jorn[$j]['hora_inicio']);
+                    $date1 = date_create($jorn[$j]['hora_fin']);
+                    echo $jorn[$j]['jornada'] . '<br>' . date_format($date, 'g:i A') . '<br>' . date_format($date1, 'g:i A');
+                        ?></td>
+                                <?php
+                            for ($i = 0; $i < count($dia); $i++) {
                                 ?>
-                            <option value="<?php echo $usu[$h]['idusuario'] ?>" 
-                                <?php if($usu[$h]['idusuario'] == $_POST["usuarios"]) echo 'selected'; ?>>
-                                    <?php echo $usu[$h]['nombres'] . " " . $usu[$h]['apellidos'] ?></option>
-                            <?php } ?>
-                        </select>-->
-        
-      
-   </tr>
 
-<tr>
-        <td align="center" width="100">Jornada</td>
+                                <td align="center" width="150" ><input style =" width: 20px;height: 20px;" class="form-control" 
+                                                                       type="checkbox" name="chequeo[<?php echo $jorn[$j]['idjornada'] . "_" . $dia[$i]['idvalor']; ?>]" 
+        <?php
+        for ($d = 0; $d < count($dispo); $d++) {
 
-            <?php
-            $dia = $ins->seldias();
-            for ($i = 0; $i < count($dia); $i++){
-            ?>
-         <td align="center" width="100"><?php echo $dia[$i]['valor']; ?></td>
-            <?php } ?>
-</tr>
+            if (($jorn[$j]['idjornada'] . "_" . $dia[$i]['idvalor']) == ($dispo[$d]['jornadaid'] . "_" . $dispo[$d]['dia'])) {
+                echo 'checked';
+            } else {
+                echo 'folse';
+            };
+            ?> 
+                                                                       <?php }; ?> value=1">  </td>  
+                                                                   <?php
+                                                                   };
+                                                               };
+                                                               ?>         
 
-<?php
+                </table>
 
-$dispo = $ins->seldisponi($usuarioid);
-          
-              
-$jorn = $ins->seljornadas();
+                <table>
+                    <tr>
+                    <a href="home.php" style="position: relative;left: 315px;top: 50px;width: 215px;"><h4>volver</h4></a>
+                    </tr>
+                    <tr>
+                        <td><input style="position: relative;left: 415px;top: 15px;width: 215px;" type="submit" name="Submit" value="Sugerir" onClick="alert('Su sujerencia fue registrada correctamente')"/></td>
+                    </tr>
+                </table>
 
-for ($j = 0; $j < count($jorn); $j++) { 
-    ?>
 
-<tr>
-    <td align="center"><?php
-            $date = date_create($jorn[$j]['hora_inicio']);
-            $date1 = date_create($jorn[$j]['hora_fin']);
-            echo $jorn[$j]['jornada'].'<br>'.date_format($date, 'g:i A').'<br>'.date_format($date1, 'g:i A');?></td>
-        <?php 
-         for ($i = 0; $i < count($dia); $i++){
-           
-            ?>
-    
-        <td align="center"><input class="form-control" type="checkbox" name="chequeo[<?php echo $jorn[$j]['idjornada']."_".$dia[$i]['idvalor'];?>]" 
-            <?php 
-    for ($d = 0; $d < count($dispo); $d++){
-        
-    if (($jorn[$j]['idjornada']."_".$dia[$i]['idvalor']) == ($dispo[$d]['jornadaid']."_".$dispo[$d]['dia']))
-        { 
-        echo 'checked' ;
-        }else{
-            echo 'folse' ;
-            };?> 
-   <?php  };?> value=1">  </td>  
-  <?php  };
-};
- 
- ?>         
-  
-</tr>
- 
-    </table>
- 
-    </table>
-    <input type="submit" name="Submit" value="Sujerir" onclick=""/>
 
-</form>
- </div>
-</body>
+
+            </form>
+        </div>
+    </body>
 </html>
 
 
